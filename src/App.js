69d3138769d6e61e159cea88c98eb2ebf02111db
Petros Devrikis <petrpsdevri@protonmail.com>
2023-01-10 13:40:15 +0200

    Add fonts and font styling

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   return (
     <>
       <Header />
-      <Nav />
       <Main />
       <Footer />
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
+import Nav from './Nav';
+
 export default function Header() {
     return (
         <header>
             <img src="./images/Logo.svg" alt="logo"/>
+            <Nav />
         </header>
     );
 }
