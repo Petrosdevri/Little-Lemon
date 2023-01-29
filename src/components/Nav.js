5d3716a9f035d08d47bbb0115733bc47e8a81262
Petros Devrikis <petrosdevri@protonmail.com>
2023-01-29 23:45:25 +0200

    Added booking form and routing files

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,36 @@
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default function Nav() {
-    return (
-        <nav>
-            <ul>
+/*<ul>
                 <li></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="/menu">Menu</a></li>
                 <li><a href="/reservations">Reservations</a></li>
                 <li><a href="/order-online">Order Online</a></li>
                 <li><a href="/login">Login</a></li>
+            </ul> */
+
+export default function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/menu">Menu</Link>
+                </li>
+                <li>
+                    <Link to="/reservations">Reservations</Link>
+                </li>
+                <li>
+                    <Link to="/order-online">Order Online</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
             </ul>
         </nav>
     );
