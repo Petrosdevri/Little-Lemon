26a79ed677905f238f7b099c81455211792f1dc6
Petrosdevri <petrosdevri@protonmail.com>
2023-01-31 16:43:25 +0200

    Updated booking form and order files

--- src/components/nav-pages/booking-page/BookingForm.js
+++ src/components/nav-pages/booking-page/BookingForm.js
@@ -1,32 +1,49 @@
 import { useState } from "react";
 
-export default function BookingForm() {
+export default function BookingForm(props) {
     const [date, setDate] = useState("");
-    const [time, setTime] = useState("");
     const [guests, setGuests] = useState("");
     const [occasion, setOccasion] = useState("");
+    const [otherOccasion, setOtherOccasion] = useState("");
+    const [finalTime, setFinalTime] = useState(
+        props.availableTimes.map((times) => <option>{times}</option>)
+    );
+
+    function handleDateChange(e) {
+        setDate(e.target.value);
+        var stringify = e.target.value;
+        const date = new Date(stringify);
+        props.updateTimes(date);
+        setFinalTime(props.availableTimes.map((times) => <option>{times}</option>));
+    }
 
     return (
-        <form style="display: grid; max-width: 200px; gap: 20px">
-            <label htmlFor="res-date">Choose date</label>
-            <input type="date" id="res-date" value={date} onChnage={e => setDate(e.target.value)} required/>
-            <label htmlFor="res-time">Choose time</label>
-            <select id="res-time " value={time} onChnage={e => setTime(e.target.value)} required>
-                <option>17:00</option>
-                <option>18:00</option>
-                <option>19:00</option>
-                <option>20:00</option>
-                <option>21:00</option>
-                <option>22:00</option>
-            </select>
-            <label htmlFor="guests">Number of guests</label>
-            <input type="number" placeholder="1" minLength="1" maxLength="10" id="guests" value={guests} onChnage={e => setGuests(e.target.value)} required/>
-            <label htmlFor="occasion">Occasion</label>
-            <select id="occasion" value={occasion} onChnage={e => setOccasion(e.target.value)} required>
-                <option>Birthday</option>
-                <option>Anniversary</option>
-            </select>
-            <input type="submit" value="Make Your reservation" />
-        </form>
+        <div class="booking-form">
+            <div class="booking-form-img">
+                <h1>Make a reservation</h1>
+                <p>Fill out the form below to make a reservation.</p>
+            </div>
+            <form class="reservations-form">
+                <label htmlFor="res-date">Choose date</label>
+                <input type="date" id="res-date" value={date} onChange={handleDateChange} required/>
+                <label htmlFor="res-time">Choose time</label>
+                <select id="res-time" required>
+                    {finalTime}
+                </select>
+                <label htmlFor="guests">Number of guests</label>
+                <input type="number" placeholder="1" minLength={1} maxLength={10} id="guests" value={guests} onChange={e => setGuests(e.target.value)} required/>
+                <label htmlFor="occasion">Occasion</label>
+                <select id="occasion" value={occasion} onChange={e => setOccasion(e.target.value)} required>
+                    <option selected>None</option>
+                    <option>Birthday</option>
+                    <option>Anniversary</option>
+                    <option>Engagement</option>
+                    <option>Other</option>
+                </select>
+                <label htmlFor="other-occasion">If you selected Other, please specify your occasion:</label>
+                <input type="text" placeholder="Other occasion" id="other-occasion" value={otherOccasion} onChange={e => setOtherOccasion(e.target.value)}/>
+                <input type="submit" value="Make Your reservation" id="submit-form-btn"/>
+            </form>
+        </div>
     );
 }
--- src/components/nav-pages/booking-page/Order.js
+++ src/components/nav-pages/booking-page/Order.js
@@ -1,1 +1,24 @@
-export default function Order() {}
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+export default function Order() {
+    function useHover(styleOnHover, styleOnNotHover) {
+        const [style, setStyle] = useState(styleOnNotHover);
+        const onMouseEnter = () => setStyle(styleOnHover)
+        const onMouseLeave = () => setStyle(styleOnNotHover)
+        return {style, onMouseEnter, onMouseLeave}
+    }
+
+    const hover = useHover({ color: '#FFFFFF'}, { color: '#000000' });
+
+    return (
+        <div class="order-online">
+            <div class="order-info">
+                <h1>Order Online</h1>
+                <p>Make sure to order online for pickup or delivery.</p>
+                <button type="submit" id="order-btn"><Link to="/reservations" {...hover}>Order Online</Link></button>
+            </div>
+        </div>
+    );
+
+}
